12ee356aabef782a4a8d2064373f639da6f789da
jujujuc <55589637+jujujuc@users.noreply.github.com>
2019-11-06 11:49:53 -0500

    a

--- List.php
+++ List.php
@@ -1,23 +1,6 @@
 <?php
 include_once 'DAL/mySQL.php';
 
-class Acteur extends SQL_Helper {
-    public $Id;
-    /** VARCHAR(48) */
-    public $Name;
-    /** VARCHAR(30) */
-    public $Country;
-    /** Date */
-    public $Birth;
-    
-    public function __construct() {
-        $this->Id = 0;          
-        $this->Name = '';
-        $this->Country = '';
-        $this->Birth = '';
-    }
-}
-
 function showContacts($acteurs) {
     if (isset($acteurs)) {
         echo '<div>';
