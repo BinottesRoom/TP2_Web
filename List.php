0bc040463277776b2e47904ac4651c617470052f
BigBinotte <55589663+BigBinotte@users.noreply.github.com>
2019-11-06 11:53:57 -0500

    yeet
    yeet

--- List.php
+++ List.php
@@ -1,41 +1,54 @@
 <?php
 include_once 'DAL/mySQL.php';
+require 'utilities/htmlHelper.php';
 
-class Acteur extends SQL_Helper {
-    public $Id;
-    /** VARCHAR(48) */
-    public $Name;
-    /** VARCHAR(30) */
-    public $Country;
-    /** Date */
-    public $Birth;
-    
-    public function __construct() {
-        $this->Id = 0;          
-        $this->Name = '';
-        $this->Country = '';
-        $this->Birth = '';
-    }
-}
 
-function showContacts($acteurs) {
+
+$content = html_open("div").html_open("b")."Favoris".html_close("b").html_close("div")
+.html_open("hr")
+        ."<div class=' bookmarks-header-layout bookmark-Header'>"
+                .html_open("div").html_close("div")
+                .html_open("div").
+                    "Nom"
+                .html_close("div")
+                .html_open("div").
+                    "Pays"
+                .html_close("div")
+                .html_open("div").
+                    "Naissance"
+                .html_close("div")
+                .html_open("div")
+                    ."<a href='AddForm.php'><img class='icon' src='images/icons/ICON_Add_Neutral.png' alt='Ajouter'>".html_close("a")
+                .html_close("div")
+        .html_close("div")
+    .html_close("hr");
+
+
+
+
+
+
+
+
+
+
+
+
+function showActeurs($acteurs) {
     if (isset($acteurs)) {
         echo '<div>';
         foreach($acteurs as $acteur){
             echo '<div>';
-            echo '<div>';
-            echo '<div>'.$acteur['Id'].'</div>';
-            echo '<div>'.$acteur['Name'].'</div>';
-            echo '<div>'.$acteur['Phone'].'</div>';
-            echo '<div>'.$acteur['Birth'].'</div>';
+                echo '<div></div>';
+                echo '<div>'.$acteur['Id'].'</div>';
+                echo '<div>'.$acteur['Name'].'</div>';
+                echo '<div>'.$acteur['Country'].'</div>';
+                echo '<div>'.$acteur['Birth'].'</div>';
             echo '</div>';
         }
         echo '</div>';
     }
 }
-
-
-
 
 require_once "MasterPage.php";
 ?>
