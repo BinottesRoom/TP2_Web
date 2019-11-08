7a8e44aa0878fdcd2b1f64a5c6da6db010765e42
BigBinotte <55589663+BigBinotte@users.noreply.github.com>
2019-11-08 08:49:46 -0500

    yeet
    yeet

--- List.php
+++ List.php
@@ -1,5 +1,5 @@
 <?php
-include_once 'DAL/mySQL.php';
+include_once 'DAL/classesDB.php';
 require 'utilities/htmlHelper.php';
 
 $content = html_open("div").html_open("b")."Favoris".html_close("b").html_close("div")
@@ -21,39 +21,6 @@ $content = html_open("div").html_open("b")."Favoris".html_close("b").html_close(
         .html_close("div")
     .html_close("hr");
 
-    foreach($ItemsInBD as $ligne)
-    {
-        $content .= "<div class='bookmarks-row-layout' >";
-        $_SESSION['idFavoris']++;
-        
-        foreach($ligne as $key2 => $value)
-        {
-            
-            if ($ligne["Id"] != $value)
-            {
-                if($value == $ligne['Url'])
-                {
-                    $content .= html_open("div");
-                    $content .= "<a href='$value'><img src='https://s2.googleusercontent.com/s2/favicons?domain=$value'>$value</a>";
-                    $content .= html_close("div");
-                }
-                else
-                {
-                    $content .= html_open("div");
-                    $content .= $value;
-                    $content .= html_close("div");
-                }
-            }
-    
-        }
-    
-        $id = $ligne['Id'];
-        $content .= html_open("div")."<a href='EditForm.php?id=$id'><img src='images/Modify.png' alt='Ajouter'>".html_close("a").html_close("div");
-        $content .= html_open("div")."<a href='DeleteForm.php?id=$id'><img src='images/Erase.png' alt='Effacer'>".html_close("a").html_close("div");
-        $content .= html_close("div");
-        $content .= "<form method='post' action='DeleteForm.php'></form>";
-        unset($value);
-    }
 
 function showActeurs($acteurs) {
     if (isset($acteurs)) {
@@ -70,6 +37,8 @@ function showActeurs($acteurs) {
         echo '</div>';
     }
 }
+
+showActeurs(Acteurs()->get());
 
 require_once "MasterPage.php";
 ?>
