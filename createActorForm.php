a8a1fab6913d03b95ceec3cd079281ce7d120352
jujujuc <55589637+jujujuc@users.noreply.github.com>
2019-11-20 10:16:20 -0500

    a

--- createActorForm.php
+++ createActorForm.php
@@ -1,7 +1,8 @@
 <?php
     include_once 'DAL/models.php';
     include_once 'utilities/redirection.php';
-    if (isset($_POST['Submit'])){
+    if (isset($_POST['Submit']))
+    {
         Actors()->createFromForm();
         redirect("listActors.php");
     }
