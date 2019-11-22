a7b3b6476119d951d874e23b28f8de8865777a79
BigBinotte <55589663+BigBinotte@users.noreply.github.com>
2019-11-22 09:00:08 -0500

    yeet

--- DAL/crudTableHelper.php
+++ DAL/crudTableHelper.php
@@ -136,9 +136,9 @@ class CRUDTableHelper{
                 foreach($this->keys as $key){
                     $html.= $this->makeCell($htmlViewRecord[$key]);
                 }
-                $html .= $this->makeEditButton($htmlViewRecord['Id'], 'Modifier '.$htmlViewRecord[$this->captionKey]);
-                $html .= $this->makeDetailsButton($htmlViewRecord['Id'], 'Détails '.$htmlViewRecord[$this->captionKey]);
-                $html .= $this->makeDeleteButton($htmlViewRecord['Id'], 'Effacer '.$htmlViewRecord[$this->captionKey]);
+            $html .= $this->makeEditButton($htmlViewRecord['Id'], 'Modifier '/*.$htmlViewRecord[$this->captionKey]*/);
+                $html .= $this->makeDetailsButton($htmlViewRecord['Id'], 'Détails '/*.$htmlViewRecord[$this->captionKey]*/);
+                $html .= $this->makeDeleteButton($htmlViewRecord['Id'], 'Effacer '/*.$htmlViewRecord[$this->captionKey]*/);
                 $html.="</div>";
             }
             $html.="</div>";
