e15f14e229e43fac77e1f01c18d8bebbb7859d2d
jujujuc <55589637+jujujuc@users.noreply.github.com>
2019-11-27 10:31:39 -0500

    a

--- DAL/models.php
+++ DAL/models.php
@@ -400,7 +400,6 @@ $viewContent .= "</form>";
                 $html .= html_header($actorHtmlViewData['BirthDate'],3);
                 $html .= html_flashButton('iconEdit',"editActorForm.php?id=$id", "éditer", "bottom");
                 $html .= html_flashButton('iconDelete',"deleteActorForm.php?id=$id", "effacer", "bottom");
-                //$allmovieitems = Casts()->CastsMoviesToItems($id);//
             $html.="</div>";
 
                        
