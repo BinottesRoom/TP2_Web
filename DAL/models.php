24ab252328b09dd5f18596a865fa63f432743cfb
jujujuc <55589637+jujujuc@users.noreply.github.com>
2019-11-27 10:34:29 -0500

    a

--- DAL/models.php
+++ DAL/models.php
@@ -349,39 +349,6 @@ final class Actors extends TableAccess{
         $html.= html_closeForm();
         return $html;
     }
-    /*
-            DB()->beginTransaction();
-        Selections()->deleteAll();//-----------------
-        if (isset($_POST['SelectedItems']))
-            saveFormSelection($_POST['SelectedItems']);
-        DB()->endTransaction();
-    // produire les items qui seront présentés dans les <select...> du formulaire
-$selectedItems = CastsActorsToItems();
-$movieItems = MoviesToItems()($selectedItems);
-
-// pruduction du html constituant le formulaire avec les deux <select...>
-$viewContent="";  
-$viewContent .= "<form method='post'>";
-    $viewContent .= "<table>";
-        $viewContent .= "<tr>";
-            $viewContent .= "<td>";
-                $viewContent .= makeSelectedList($selectedItems);
-            $viewContent .= "</td>";
-            $viewContent .= "<td>";
-                // il faut utiliser le fichier css/flashButtons.css pour 
-                // que les <div> de classes MoveLeft et MoveRight se comportent
-                // comme des boutons flash
-                $viewContent .= "<div id='MoveLeft'  ></div>"; 
-                $viewContent .= "<div id='MoveRight' ></div>";
-            $viewContent .= "</td>";
-            $viewContent .= "<td>";
-                $viewContent .= makeUnselectedList($movieItems, $selectedItems);
-            $content .= "</td>";
-        $viewContent .= "</tr>";
-    $viewContent .= "</table>";
-    $viewContent .= "<input type='submit' name='submit' value='Enregistrer'>";
-$viewContent .= "</form>";
-?>*/
 
     public function getDetailsHtml($id){
         $actorRecord = $this->get($id);
